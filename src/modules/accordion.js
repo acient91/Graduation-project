01afc7735a0102571cc8857c86634cf2cf0cfb8f
Alexandr Makarov <speqtr.91@gmail.com>
2022-02-13 11:44:18 +0300

    добавил функционал открытия модального окна, прокрутки страницы на верх по клику на стрелку

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -1,5 +1,22 @@
 const accordion = () => {
   const accordionItem = document.querySelectorAll('.accordeon-item');
+
+  const active = document.getElementsByClassName('accordeon-item--active');
+  console.log(accordionItem);
+  console.log(active);
+
+  accordionItem.forEach((item, i, accordionItem) => {
+    item.addEventListener('click', () => {
+      if (active.length > 0 && active[0] !== this) {
+        active[0].classList.remove('accordeon-item--active');
+
+      } else {
+        this.classList.toggle('accordeon-item--active')
+      }
+    })
+  })
+
+
   const accordeonContent = document.querySelectorAll('.accordeon-content');
 
 
